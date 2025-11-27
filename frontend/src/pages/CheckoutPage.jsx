2b34c574b8c44bcb2802b8076fed3411c1d72489
lami216 <iiyad21694@gmail.com>
2025-11-27 20:24:03 -0100

    Update WhatsApp checkout number

--- frontend/src/pages/CheckoutPage.jsx
+++ frontend/src/pages/CheckoutPage.jsx
@@ -142,7 +142,7 @@ const CheckoutPage = () => {
                         requestPayload.couponCode = coupon.code;
                 }
 
-                const STORE_WHATSAPP_NUMBER = "22231117700";
+                const STORE_WHATSAPP_NUMBER = "22232167777";
 
                 setIsSubmitting(true);
 
